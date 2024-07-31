e572ce992d5c01da19f5b466e0fdc72b3f1859d0
bano1397 <banozahid1397@gmail.com>
2024-08-01 02:09:59 +0500

    first commit

--- API.js
+++ API.js
@@ -1,32 +1,29 @@
-const express = require('express');
 const dbConnect = require('./mongodb');
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/', async (req, resp) => {
     try {
         let data = await dbConnect();
-        let result = await data.find().toArray();
-        res.send(result);
+        data = await data.find().toArray();
+        resp.send(data);
     } catch (error) {
-        console.error(error);
-        res.status(500).send("Error fetching data");
+        resp.status(500).send({ error: 'An error occurred while fetching data' });
     }
 });
 
-app.post("/", async (req, res) => {
+app.post("/", async (req, resp) => {
     try {
         let data = await dbConnect();
-        let result = await data.insertMany(req.body);
-        res.send(result);
+        let result = await data.insertOne(req.body);
+        resp.send(result);
     } catch (error) {
-        console.error(error);
-        res.status(500).send("Error inserting data");
+        resp.status(500).send({ error: 'An error occurred while inserting data' });
     }
 });
 
-app.listen(5000, () => {
+app.listen(5001, () => {
     console.log('Server is running on port 5000');
 });
-    
